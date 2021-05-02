0b8b38fc5ec652d11aa5f2051de5ab3fffe153bd
Cesar-Acevedo23 <ces.acevedo.23@gmail.com>
2021-05-02 17:38:54 -0400

    landing-page finalizado

--- landing-page/src/components/navBar.js
+++ landing-page/src/components/navBar.js
@@ -1,19 +1,15 @@
 import React from "react";
-import {Navbar, Form, FormControl, Button, Nav} from 'react-bootstrap'
+import {Navbar, Nav} from 'react-bootstrap'
 
 const Nav1 = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar className="d-flex" bg="dark" variant="dark">
       <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
+      <Nav className="mr-auto p-2">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
         <Nav.Link href="#pricing">Pricing</Nav.Link>
       </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-info">Search</Button>
-      </Form>
     </Navbar>
   );
 };
